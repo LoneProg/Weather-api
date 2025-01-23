2d78b068080af4d35dbf356b911b45bae01dd166
loneprog <wolfdon73@gmail.com>
2025-01-23 10:53:14 +0000

    initialize weather API with Express, Axios, and dotenv; add .gitignore and package.json

---
+++ src/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const env = require('dotenv');
+const axios = require('axios');
+env.config();
+const app = express();
+const port = process.env.PORT || 3000;
+const requestIp = require('request-ip');
+const morgan = require("morgan");
+
+app.use(express.json())
+app.use(requestIp.mw())
+app.use(morgan(':method :url :response-time'))
+app.set('trustproxy', true);
+app.enable('trustproxy');
+
+app.get('/', (req, res) => {
+    res.send("wELCOME TO THE WEATHER API");
+});
+
+app.get('/weather', async (req, res) => {
+    try {
+        const name =req.query.name;
+        const ip = req.clientIp;
+
+        if (!name) {
+            return res.status(404).json({ message:'please provide a name'})
+        }
+
+        if (ip === "::1" || ip === "127.0. 0.1") {
+            return res.status(404).json({ message:"ip address is not available"})
+        }
+
+        const response = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.GEOLOCATION_KEY}&ip=${ip}`)
+
+        const lat = response.data.latitude;
+        const lng = response.data.longitude;
+
+        const weatherResponse = await axios.get(` http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_KEY}&q=${lat},${lng}`);
+        // console.log(weatherResponse);
+
+        const temp = weatherResponse.data.current.temp_c
+        const location = weatherResponse.data.location.region;
+         //console.log(temp);
+        // console.log(location);
+
+        const data = {
+            
+           message: `Hello ${name} from ${location} your weather condition is ${temp}`
+
+        }
+        return res.status(200).json({message: "sucess", data});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: 'error fetching weather data'})
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+});
